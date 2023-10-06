11fecbfaa9595526e0bf4dcb22e7d87577b44a49
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2023-10-06 20:25:00 +0300

    The last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - 'main' entry point
+ *@void: empty parameter list
+ *Description: prints the alphabet in lower case
+ *int main(void): the main function doesn't accept any parameters
+ *Return: always 0 (success)
+ */
+int main(void)
+{
+	putchar("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
