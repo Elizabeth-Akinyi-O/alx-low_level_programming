1f7381b9647bb0cfca1439a6e7967c1f05123159
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2023-10-12 02:18:52 +0300

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - adds two integers and returns the result
+ *@a: first number to be added
+ *@z: second number to be added
+ *Return: sum
+ */
+int add(int a, int z)
+{
+	int sum = (a + z);
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main'h"
+/**
+ *times_table - prints the 9 times table, starting with 0
+ *
+ *Return: void
+*/
+ void times_table(void)
+{
+	int i;
+	int j;
+	int k:
+	int l;
+	int m:
+
+	for (j = 0; j <= ; j)
+	{
+		for (i = 0; i <= 9 ; i++)
+		{
+			_putchar();
+			_putchar(',');
+		}
+		_putchar(',');
+		_putchar('.');
+		_putchar('.');
