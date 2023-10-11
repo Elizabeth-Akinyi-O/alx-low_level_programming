f5fb7944bebf7c83282bd19296acc8e06b29f06c
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2023-10-11 19:14:11 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+int _putchar(char);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
