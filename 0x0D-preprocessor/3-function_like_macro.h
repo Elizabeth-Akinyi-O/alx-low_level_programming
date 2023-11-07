8c13de828a6954ed7faa50f3524c279427552a4f
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2023-11-07 03:19:15 +0300

    Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef ABS_HEADER
+#define ABS_HEADER
+/**
+ *ABS - computes the absolute value of a number x
+ *@x: input value
+ *Description: if x is -ve, then multiply x with -1
+ *to make it positive, else print x
+ */
+#define ABS(x) ((x) < 0 ? (-1) * (x) : (x))
+
+#endif /* 3-function_like_macro.h file ends here */
