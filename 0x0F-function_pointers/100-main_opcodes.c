54e0703ce97b19daf9a83e0aed9173aa3451e2b2
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2023-11-10 01:42:03 +0300

    Correction

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ *print_opcodes - prints opcodes
+ *@addr: address of the main function
+ *@bytes: number of bytes to print
+ *Return: void
+ */
+void print_opcodes(char *addr, int bytes)
+{
+	int a;
+
+	for (a = 0; a < bytes; a++)
+	{
+		printf("%.2hhx", addr[a]);
+		if (a < bytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+}
+/**
  *main - prints the opcodes of its own main function
  *@argc: count
  *@argv: vector
  *Return: 0
  */
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
-	int bytes, index;
-	int (*address)(int, char **) = main;
-	unsigned char opcode;
+	int bytes;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	
 	bytes = atoi(argv[1]);
-
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
-	for (index = 0; index < bytes; index++)
-	{
-		opcode = *(unsigned char *)address;
-		printf("%.2x", opcode);
-
-		if (index == bytes - 1)
-			continue;
-		printf(" ");
-
-		address++;
-	}
-	printf("\n");
+	print_opcodes((char *)&main, bytes);
 	return (0);
 }
