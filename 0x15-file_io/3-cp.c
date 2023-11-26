e05c24e8c5f796774b626d090b5d5d55dcf36441
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2023-11-26 10:22:00 +0300

    Copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
