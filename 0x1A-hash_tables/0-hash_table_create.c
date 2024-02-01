9f59e4d6b26eed29ff71c526011b47bba120fa42
Elizabeth-Akinyi-O <lizbethakinyio@gmail.com>
2024-02-01 11:33:06 +0300

    Creates a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,19 +7,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *ht;
-        unsigned long int a;
+	hash_table_t *ht;
+	unsigned long int a;
 
-        ht = malloc(sizeof(hash_table_t));
-        if (ht == NULL)
-                return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-        ht->size = size;
-        ht->array = malloc(sizeof(hash_node_t *) * size);
-        if (ht->array == NULL)
-                return (NULL);
-        for (a = 0; a < size; a++)
-                ht->array[a] = NULL;
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (a = 0; a < size; a++)
+		ht->array[a] = NULL;
 
-        return (ht);
+	return (ht);
 }
